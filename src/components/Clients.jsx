1ad3c54803de3590f537db0d1933971ec05c1c58
yeasin2448 <ayeasin108@gmail.com>
2023-06-21 17:33:42 +0600

    1st up

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
 function Clients() {
   return (
-    <div>
-      Clients
-    </div>
-  )
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] cursor-pointer`}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="sm:w-[192px] object-contain hover:mix-blend-lighten"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Clients
+export default Clients;
