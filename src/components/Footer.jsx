7f7e603f80b9228c76b796eeb576f01318e51111
yeasin2448 <ayeasin108@gmail.com>
2023-06-22 11:35:01 +0600

    1st up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import styles from "../style";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
-}
+  return <div>Footer</div>;
+};
 
-export default Footer
+export default Footer;
